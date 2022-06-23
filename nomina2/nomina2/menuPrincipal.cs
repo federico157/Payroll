8642f6842f8aeb859f7e45aee933079cf5330974
federico157 <104031924+federico157@users.noreply.github.com>
2022-06-23 14:44:26 -0500

    Add files via upload

---
+++ nomina2/nomina2/menuPrincipal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nomina2
+{
+    internal class MenuPrincipal
+    {
+        public static void Main(string[] args)
+        {
+            Exercises Operations = new Exercises();
+
+            Console.WriteLine("Main payroll");
+            Console.WriteLine("1. Insert new employee");
+            Console.WriteLine("2. Exit the main");
+
+            string Opc = Console.ReadLine();
+
+            int Opc1;
+
+            switch(Opc)
+            {
+
+                case "1":
+                    do
+                    {
+                        Operations.CalculatePayroll();
+                        Operations.PrintDates();
+                        Console.WriteLine("1. Insert new employee");
+                        Console.WriteLine("2. Exit ");
+
+                        Opc1 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Data invalied");
+
+                        Console.Clear();
+                    }
+                    while (Opc1 != 2);
+                    break;
+
+                    case "2":
+                    Console.WriteLine("Exit");
+                    break;
+
+                    default:
+                    Console.WriteLine("Exit of Console");
+                    break;
+            }
+
+
+
+        }
+    }
+}
